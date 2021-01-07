29b1fe6cf2efd6d6bbbb865e78358f41df6013ea
Wasili Adamow <wasili.adamow@gmail.com>
2021-01-07 12:33:06 +0100

    Disable native driver for label animation

--- src/components/field/index.js
+++ src/components/field/index.js
@@ -236,7 +236,7 @@ export default class TextField extends PureComponent {
 
     let options = {
       toValue: this.labelState(),
-      useNativeDriver: true,
+      useNativeDriver: false,
       duration,
     };
 
